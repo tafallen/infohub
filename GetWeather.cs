4cbbbdfea1337af9fdc35343dd370e9de9b1fdef
Tim Allen <tim@timallen.me.uk>
2020-06-05 20:43:04 +0100

    Implemented GetWeather function

--- GetWeather.cs
+++ GetWeather.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using RestSharp;
 
 namespace uk.me.timallen.infohub
 {
@@ -17,19 +18,61 @@ namespace uk.me.timallen.infohub
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
+            string location = req.Query["location"];
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            location = location ?? data?.location;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            log.LogInformation("location: " + location);
 
-            return new OkObjectResult(responseMessage);
+            var response = GetFromAccuweather(location);
+
+            return new OkObjectResult(response);
+        }
+
+        private static string GetFromAccuweather(string location)
+        {
+            var response = MakeRequest(location);
+            dynamic weather = JsonConvert.DeserializeObject(response.Content);
+            return FormatResponse(weather["DailyForecasts"]);
+        }
+
+        private static string FormatResponse(dynamic dailyForecasts)
+        {
+            string result = string.Empty;
+
+            for(int i = 0; i < dailyForecasts.Count; i++)
+            {
+                dynamic day = dailyForecasts[i];
+                var  x =  "{ \"day\": \"" + DateTime.Now.AddDays(i).DayOfWeek.ToString() + 
+                    "\", \"min\": \"" + day.Temperature.Minimum.Value + 
+                    "\", \"max\": \"" + day.Temperature.Maximum.Value + 
+                    "\", \"summary\":\"" + day.Day.IconPhrase + 
+                    "\", \"dayIcon\": \"" + day.Day.Icon + 
+                    "\", \"nightIcon\": \"" + day.Night.Icon + 
+                    "\"},";
+
+                result += x;
+            }
+            result = result.Substring(0, result.Length-1);
+            return $"[{result}]";
+        }
+
+        private static IRestResponse MakeRequest(string location)
+        {
+            var client = GetRestClient(location);
+            var request = new RestRequest(Method.GET);
+            return client.Execute(request);
+        }
+
+        private static RestClient GetRestClient(string location)
+        {
+            var serviceApi = Environment.GetEnvironmentVariable("weatherapi");
+            var serviceKey = Environment.GetEnvironmentVariable("weatherapikey");
+            return new RestClient(serviceApi + location +"?apikey="+ serviceKey +"&details=false&metric=true")
+            {
+                Timeout = -1
+            };
         }
     }
 }
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # infohub
+
 (A centralised group of Azure Functions that feed an infoscreen)
 
 ## The APIs
 
 ### GetSunTimes
 
-#### Description
 Get the sunset and sunrise times for a given lattitude and longitude.
 
 #### Configuration
@@ -13,36 +13,53 @@ Get the sunset and sunrise times for a given lattitude and longitude.
 To use this you'll need to set a environment variable containing the URI of the sunrise-sunset.org API. It's a free service. They're good people.
 
 ##### Local config
+
 In your local.settings.json file add this in the values section:
 ```"sunrisesunsetapi" : "https://api.sunrise-sunset.org/json"```
 
 ##### Server side setup
+
 In the Application Settings area of the Azure Functions where you're deploying this you'll need to add a new setting with the name _sunrisesunsetapi_ and the URI as shown above.
 
 We'll be using this for all the API URLs and API Keys as it's bad practice to put them into config files.
 Ideally we'd secure them in KeyVault but that's for another day.
 
 #### Usage
+
 Provide the Query Params _lat_ and _lng_ with numerical values for for lattitude and longitude. So you'd call it like this from within Postman or from your code:
 ```https://<<your service URI>>/api/GetSunTimes?lat=<<your lattitude>>&lng=-<<your longitude>>```
 Change the details within the <<>> to suit your service and location. And get rid of the <<>> of course.
-
-
+Returned is a small scrap of JSON with the sunrise and sunset times for the chosen location.
 
 ### GetWeather
 
-#### Description
+Gets the weather for a given location.
+
+#### Configuration
+
+This uses the Accuweather weather API from [https://developer.accuweather.com/](https://developer.accuweather.com/) so an account with them and an API key is required. A location key is required to get a forecast for the chosen area. Location keys can be determined using this api: [https://developer.accuweather.com/accuweather-locations-api/apis](https://developer.accuweather.com/accuweather-locations-api/apis).
+
+##### Local config
+
+In your local.settings.json file add this in the values section:
+```"weatherapi" : "http://dataservice.accuweather.com/forecasts/v1/daily/5day/"```
+```"weatherapikey":"<<PUT YOUR API KEY HERE>>"```
+
+##### Server side setup
+
+In the Application Settings area of the Azure Functions where you're deploying this you'll need to add a new setting with the name _weatherapi_ and the URI as shown above. You'll also need to add _weatherapikey_ and the api key.
 
 #### Usage
 
-[TODO: Add example]
+Provide the Query Param _location__ with the location key and you'd call it like this from within Postman or from your code:
+```https://<<your service URI>>/api/GetWeather??location=<<Your Location Key>>```
+
+Returned is a JSON object containing a basic 5 day weather forecast, maximum and minimum temperatures and a summary forecast.
 
 #### To Do
-
+[TODO: Cache results for 12 hours]
 
 ### GetNews
-
-#### Description
 
 #### Usage
 
@@ -53,16 +70,11 @@ Change the details within the <<>> to suit your service and location. And get ri
 
 ### GetHeating
 
-#### Description
-
 #### Usage
 
 [TODO: Add example]
 
 #### To Do
-
-
-
 
 ## The Code
 
